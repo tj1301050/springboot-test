5444c122e6cb4dfc660d261e93b09f0762e9c869
tianjian <2279683269@qq.com>
2021-10-22 18:20:57 +0800

    配置文件修改

--- demo/src/main/java/com/hdedu/DemoApplication.java
+++ demo/src/main/java/com/hdedu/DemoApplication.java
@@ -3,12 +3,10 @@ package com.hdedu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
 
 @SpringBootApplication
 @MapperScan("com.hdedu.mapper")
+//@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
